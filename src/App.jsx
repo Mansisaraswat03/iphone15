0c5b2e2e3275c73610513c195de6791bd9318cf6
Mansi Saraswat <mansisaraswat030@gmail.com>
2024-04-26 00:09:58 +0530

    header completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Hero, Highlights, Navbar} from './components'
+
+function App() {
+ 
+  return (
+    <main className='bg-black'>
+    <Navbar/>
+    <Hero/>
+    <Highlights/>
+    </main>
+  )
+}
+
+export default App
